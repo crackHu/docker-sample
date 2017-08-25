e92780aeb11ebcefccf221a1dc323eebf4055a31
crackHu <296717367@qq.com>
2017-08-25 17:58:21 +0800

    init docker sample

---
+++ src/main/java/club/eval/dockersample/DockerSampleApplication.java
@@ -0,0 +1,20 @@
+package club.eval.dockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockerSampleApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSampleApplication.class, args);
+	}
+}
